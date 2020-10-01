ab31c5bfb139a3b9d05b47be4ee431fd606ff846
Shein92 <almashij92@gmail.com>
2020-10-01 15:16:30 +0300

    small fixes

--- src/ui/Login/Login.tsx
+++ src/ui/Login/Login.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '../Common/Button/Button';
+import Input from '../Common/Input/Input';
 import style from './Login.module.css';
 
 type LoginPropType = any
@@ -7,6 +9,12 @@ const Login = (props: LoginPropType) => {
 	return (
 		<div className={style.login}>
 			<h2>Login Page</h2>
+			<div>
+				<Input value={'Choose the dark side'} onChange={(text: string) => console.log(text)} />
+			</div>
+			<div>
+				<Button text={'we have cookies!:)'} onClick={() => alert('You have chosen wisely')} />
+			</div>
 		</div>
 	)
 }
