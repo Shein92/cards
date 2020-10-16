916ea7c99d39462f86b628b7be31f3a8b674d6c4
Shein92 <almashij92@gmail.com>
2020-10-16 13:01:37 +0300

    small fix

--- src/ui/PageNotFound/PageNotFound.tsx
+++ src/ui/PageNotFound/PageNotFound.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
 import style from './PageNotFound.module.css';
 
 const PageNotFound = () => {
-    return (
-        <div className={style.pageNotFound}>
-            <h2>
-                <div>
-                    <span>PAGE NOT FOUND</span>
-                </div>
-                <div>
-                    <span>404 ERROR!</span>
-                </div>
-            </h2>
-        </div>
-    )
+
+	if (<Route exact path={'/'} />) {
+		return <Redirect to={'/login'} />
+	}
+
+	return (
+		<div className={style.pageNotFound}>
+			<h2>
+				<div>
+					<span>PAGE NOT FOUND</span>
+				</div>
+				<div>
+					<span>404 ERROR!</span>
+				</div>
+			</h2>
+		</div>
+	)
 }
 
 export default PageNotFound;
