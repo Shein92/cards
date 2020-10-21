c1bd7497e62618e9aecf9baddb0acb01559d64ea
Bondarev-Igor <tosca@tut.by>
2020-10-21 23:14:04 +0300

    add applyMiddleware

--- src/bll/registration-reducer.ts
+++ src/bll/registration-reducer.ts
@@ -6,31 +6,40 @@ export type RegistrationStateType = any
 export type ActionsType = any;
 
 const initialState: RegistrationStateType = {
-	isRegistred: false
+    isRegistred: false
 }
 
 type InitialStateType = typeof initialState
 
 export const registrationReducer = (state: RegistrationStateType = initialState, action: ActionsType): InitialStateType => {
-	switch(action.type) {
-		case 'CREATE-REGISTRATION': {
-            return {...state, isRegistred: action.value }
-		}
-		default: {
-			return state
-		}
-	}
+    switch (action.type) {
+        case 'CREATE-REGISTRATION': {
+            return {...state, isRegistred: action.value}
+        }
+        default: {
+            return state
+        }
+    }
 }
 
 // actions
 export const registrationAC = (value: boolean) =>
-	({type: 'CREATE-REGISTRATION', value} as const)
+    ({type: 'CREATE-REGISTRATION', value} as const)
 
 // thunks
 export const registrationTC = (data: RegistrationParamsType) => (dispatch: Dispatch) => {
-	registerAPI.registration(data)
-		.then(res => {
-				dispatch(registrationAC(true))
-			}
-		)
+    debugger
+    registerAPI.registration(data)
+        .then(res => {
+                debugger
+                dispatch(registrationAC(true))
+            }
+        )
+        .catch((err) => {
+            debugger
+        })
+}
+
+export const test111 = () => () => {
+    debugger
 }
